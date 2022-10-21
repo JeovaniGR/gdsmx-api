378e3e546f36aa71f99d9b5dbdf14d3fb71fd242
RaulDuarteBeltran97 <Raul.Duarte1@gds.ey.com>
2022-10-21 10:04:30 -0500

    feat: Add EmployeeBadge Request Class

---
+++ DTO/RequestEmployeeBadge.cs
@@ -0,0 +1,18 @@
+﻿namespace gdsmx_back_netcoreAPI.DTO
+{
+    public class RequestEmployeeBadge
+    {
+        public int IdEmployee { get; set; }
+        public string? Level { get; set; }
+        public string? Status { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+
+        public RequestEmployeeBadge()
+        {
+            IdEmployee = 0;
+            Page = 1;
+            PageSize = 1;
+        }
+    }
+}
