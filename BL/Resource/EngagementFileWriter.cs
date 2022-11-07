25211a1ba22fbe1fc5f9811a3812ab8f30881224
RaulDuarteBeltran97 <Raul.Duarte1@gds.ey.com>
2022-11-07 12:39:11 -0600

    feat: Add headers to EngagementFileWriter

---
+++ BL/Resource/EngagementFileWriter.cs
@@ -0,0 +1,25 @@
+﻿namespace gdsmx_back_netcoreAPI.BL.Resource
+{
+    public class EngagementFileWriter
+    {
+        private readonly List<string> header = new List<string>()
+        {
+            "GPN",
+            "FirstName",
+            "MiddleName",
+            "SecondLastName",
+            "EngagementId",
+            "CustomerName",
+            "ProyectName",
+            "EngagementHours",
+            "CancelationDate",
+            "Comments",
+            "StartDate",
+            "EndDate",
+            "ProjectManagerName",
+            "ProjectManagerEmail",
+            "Status",
+            "Description"
+        };
+    }
+}
